54c475e44e9b0fb8001a7a8f605486873b0473e8
firebingo <matthew.t.kides@gmail.com>
2018-04-21 13:27:04 -0700

    min js source map. Moved generic css. removed fonts.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify-es').default;
+const sourcemaps = require('gulp-sourcemaps');
 
 
 const jsFiles = [
@@ -16,9 +17,11 @@ const jsDest = "Js";
 gulp.task('minify', function() {
   // place code for your default task here
   return gulp.src(jsFiles)
+		.pipe(sourcemaps.init())
         .pipe(concat('XduPlayer.js'))
         .pipe(gulp.dest(jsDest))
         .pipe(rename('XduPlayer.min.js'))
         .pipe(uglify())
+		.pipe(sourcemaps.write(''))
         .pipe(gulp.dest(jsDest));
 });
